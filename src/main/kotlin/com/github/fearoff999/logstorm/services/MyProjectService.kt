07129f7ebc9280232c4e8f6535b14c3e91ca51a6
GitHub Action <action@github.com>
2021-08-02 10:54:38 +0000

    Template cleanup

--- src/main/kotlin/com/github/fearoff999/logstorm/services/MyProjectService.kt
+++ src/main/kotlin/com/github/fearoff999/logstorm/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.fearoff999.logstorm.services
 
+import com.github.fearoff999.logstorm.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
